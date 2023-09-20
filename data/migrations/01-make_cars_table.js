333cf4803c622645899ed55bffb55a6d24ca7667
Victoria Blancas <vikki6688@gmail.com>
2023-09-20 16:04:09 -0400

    wip

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,12 +1,12 @@
 exports.up = function (knex) {
   return knex.schema.createTable('cars', tbl => {
     tbl.increments();
-    tbl.text('vin').unique().notNullable();
-    tbl.text('make').notNullable();
-    tbl.text('model').notNullable();
-    tbl.text('mileage').decimal().notNullable();
-    tbl.text('title');
-    tbl.text('transmission');
+    tbl.string('vin', 13).unique().notNullable();
+    tbl.text('make', 128).notNullable();
+    tbl.text('model', 128).notNullable();
+    tbl.text('mileage').numeric().unsigned().notNullable();
+    tbl.text('title', 128);
+    tbl.text('transmission', 128);
   })
 };
 
